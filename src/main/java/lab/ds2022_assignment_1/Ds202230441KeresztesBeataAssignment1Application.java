a10149465233db30373a0380332f484887e1b76f
Keresztes Beata <keresztesbeata00@yahoo.com>
2022-10-05 23:55:20 +0300

    initial commit

---
+++ src/main/java/lab/ds2022_assignment_1/Ds202230441KeresztesBeataAssignment1Application.java
@@ -0,0 +1,13 @@
+package lab.ds2022_assignment_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds202230441KeresztesBeataAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ds202230441KeresztesBeataAssignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/lab/ds2022_assignment_1/Ds202230441KeresztesBeataAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.ds2022_assignment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds202230441KeresztesBeataAssignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
